99c5e6b5234fa8f625640a7c7487830ec106ca6e
OmarMWarraich <earthling986@gmail.com>
2025-07-15 14:44:27 +0500

    feat: add migrations with goose
    - Add goose migration support with embedded SQL files
    - Create database schema for users, workouts, and workout_entries tables
    - Add migration filesystem embedding for deployment
    - Include proper foreign key relationships and constraints

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/OmarMWarraich/femProject/internal/api"
 	"github.com/OmarMWarraich/femProject/internal/store"
+	"github.com/OmarMWarraich/femProject/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return  nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
